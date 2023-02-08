b54859813520aec5ec313191ffeb43bca99387eb
Milad <milad@MBP-von-Milad.fritz.box>
2023-02-08 21:46:53 +0100

    initial commit

---
+++ src/pages/About.js
@@ -0,0 +1,25 @@
+export default function About() {
+  return (
+    <div className="about">
+      <h2>About Us</h2>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Sunt itaque
+        magnam hic mollitia quas ipsa maxime, consectetur laboriosam, sit neque
+        reiciendis praesentium tempora aspernatur earum nobis officiis incidunt
+        odit placeat?
+      </p>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Sunt itaque
+        magnam hic mollitia quas ipsa maxime, consectetur laboriosam, sit neque
+        reiciendis praesentium tempora aspernatur earum nobis officiis incidunt
+        odit placeat?
+      </p>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Sunt itaque
+        magnam hic mollitia quas ipsa maxime, consectetur laboriosam, sit neque
+        reiciendis praesentium tempora aspernatur earum nobis officiis incidunt
+        odit placeat?
+      </p>
+    </div>
+  );
+}
---
+++ src/pages/Home.js
@@ -0,0 +1,25 @@
+export default function Home() {
+  return (
+    <div className="home">
+      <h2>Welcome</h2>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Sunt itaque
+        magnam hic mollitia quas ipsa maxime, consectetur laboriosam, sit neque
+        reiciendis praesentium tempora aspernatur earum nobis officiis incidunt
+        odit placeat?
+      </p>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Sunt itaque
+        magnam hic mollitia quas ipsa maxime, consectetur laboriosam, sit neque
+        reiciendis praesentium tempora aspernatur earum nobis officiis incidunt
+        odit placeat?
+      </p>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Sunt itaque
+        magnam hic mollitia quas ipsa maxime, consectetur laboriosam, sit neque
+        reiciendis praesentium tempora aspernatur earum nobis officiis incidunt
+        odit placeat?
+      </p>
+    </div>
+  );
+}
